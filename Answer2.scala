42a7868927724cf8d5e84e151d3deaafb4294639
uekan-source <is.hrsrs05@gmail.com>
2026-06-04 17:34:21 +0900

    Update Answer2.scala

--- Answer2.scala
+++ Answer2.scala
@@ -2,8 +2,8 @@ package education.section2
 
 object Answer3:
   def main(args: Array[String]): Unit =
-    println(optionQuestion3(Some(5))
+    println(optionQuestion3(Some(5)))
     println(optionQuestion3(None))
 
-  def optionQuestion3(num: Option[Int]): Option[Int]) =
+  def optionQuestion3(num: Option[Int]): Option[Int] =
     num.map(i => i + 3)
