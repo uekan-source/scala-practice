b12cc54ee758fbe9a1627c29cc729307d8a3e145
名前       : (例) Kanta Ueno <k.ueno@hybrid-technologies.co.jp>
2026-06-15 16:34:45 +0900

    Answer

---
+++ src/main/scala/work/scala/Answer14.scala
@@ -0,0 +1,31 @@
+package work.scala
+
+// 種類（suit: ♠ ♥ ♦ ♣ などのマーク）と数字（number）を持つ 1 枚のカード
+case class Card(suit: String, number: Int)
+
+object Answer14:
+  def main(args: Array[String]): Unit =
+    val hand: Seq[Card] = Seq(
+      Card("♠", 10),
+      Card("♥", 3),
+      Card("♦", 7)
+    )
+
+    println(sortedLabels(hand))
+    println(totalNumber(hand))
+
+  /**
+   * 数字の小さい順に並べてラベルにする
+   */
+  def sortedLabels(cards: Seq[Card]): Seq[String] =
+    cards
+      .sortBy(card => card.number)
+      .map(card => s"${card.suit}${card.number}")
+
+  /**
+   * 数字の合計を求める
+   */
+  def totalNumber(cards: Seq[Card]): Int =
+    cards
+      .map(num => num.number)
+      .sum
