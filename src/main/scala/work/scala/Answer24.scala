f843f7678c6ee7966b7f74ca74d0a926b6765d47
上野寛太 <is.hrsrs05@gmail.com>
2026-06-27 17:50:50 +0900

    Answer

--- src/main/scala/work/scala/Answer24.scala
+++ src/main/scala/work/scala/Answer24.scala
@@ -14,7 +14,7 @@ object Answer24:
   enum WithdrawError:
     case NotFoundId(id: Int)
     case IncorrectAmount(amount: Int)
-    case OutOfBalance
+    case OutOfBalance(balance: Int, amount: Int)
 
   def main(args: Array[String]): Unit = 
   
@@ -25,23 +25,42 @@ object Answer24:
         Account(3, "鈴木", 12000)
       )
 
-    val accountsByid :Map[Int, Account] =
+    val accountsById :Map[Int, Account] =
       accounts
         .map(account => account.id -> account)
         .toMap
 
-    println(confirmBalance(Account(1, "田中",  5000), 3000)
-    println(confirmBalance(Account(1, "田中",  5000),    0)
-    println(confirmBalance(Account(2, "佐藤",     0),  100)
+    println(confirmBalance(Account(1, "田中",  5000), 3000))
+    println(confirmBalance(Account(1, "田中",  5000),    0))
+    println(confirmBalance(Account(2, "佐藤",     0),  100))
+    println(Await.result(serchAccount(accountsById,  1), Duration.Inf))
+    println(Await.result(serchAccount(accountsById, 99), Duration.Inf))
+
+
 
 
   def confirmBalance(account: Account, amount: Int): Either[WithdrawError, Int] =
     if amount <= 0 then
       Left(WithdrawError.IncorrectAmount(amount))
     else if account.balance < amount then
-      Left(WithdrawError.OutOfBalance)
+      Left(WithdrawError.OutOfBalance(account.balance, amount))
     else 
       Right(account.balance - amount)
+
+  def serchAccount(accountsById :Map[Int, Account], id: Int): Future[Either[WithdrawError, Account]] =
+    Future:
+      Thread.sleep(1000)
+      accountsById
+        .get(id)
+        .toRight(WithdrawError.NotFoundId(id))
+
+  def processWithdraw(
+    sourceId: Int,
+    targetId: Int,
+    amount:   Int,
+    accountsById :Map[Int, Account]
+  ):Future[Either[WithdrawError, Int, Int] =
+
 
 
 
