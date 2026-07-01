a94d4130d504fa695cb682364860e682ecafbd03
名前       : (例) Kanta Ueno <k.ueno@hybrid-technologies.co.jp>
2026-07-01 10:53:58 +0900

    Answer

--- src/main/scala/work/scala/Answer29.scala
+++ src/main/scala/work/scala/Answer29.scala
@@ -2,10 +2,12 @@ package work.scala
 
 object Answer29:
   def main(args: Array[String]): Unit =
-    println(Counter(0).increment.increment.count)
+    println(divChain(20, 2, 5))
+    println(divChain(20, 0, 5))
+    println(divChain(20, 2, 0))
 
-  case class Counter(count: Int)
+  def divide(a: Int, b: Int): Option[Int] =
+    if b != 0  then Some(a / b) else None
 
-  def increment(count: Int): Counter =
-    Counter(count + 1)
-
+  def divChain(a: Int, b: Int, c: Int): Option[Int] =
+    divide(a, b).flatMap(ab => divide(ab, c))
