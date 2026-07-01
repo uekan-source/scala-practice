0472792699bac17c8bc82e0fc6b10bca5ca20e15
名前       : (例) Kanta Ueno <k.ueno@hybrid-technologies.co.jp>
2026-07-01 19:03:20 +0900

    Answer

---
+++ src/main/scala/work/scala/Answer36.scala
@@ -0,0 +1,20 @@
+package work.scala
+
+object Answer36:
+
+  case class Item(id: Int, name: String, price: Int)
+
+  def main(args: Array[String]): Unit =
+    println(items = Seq(Item(1, "ペン", 100), Item(2, "ノート", 200)), id1 = 1, id2 = 2)
+    println(items = Seq(Item(1, "ペン", 100), Item(2, "ノート", 200)), id1 = 1, id2 = 99)
+
+  def findId(items: Seq[Item], targetId: Int): Either[String, Item] =
+    items
+      .find(_.id == targetId)
+      .toRight("Idが見つかりません")
+
+  def totalPrice(items: Seq[Item], id1: Int, id2: Int): Either[String, Int] =
+    for {
+      a <- findId(items, id1)
+      b <- findId(items, id2)
+    } yield a + b
