5e5c9a7b22b9313df3b0316cad71c095195fb63b
名前       : (例) Kanta Ueno <k.ueno@hybrid-technologies.co.jp>
2026-07-08 15:23:23 +0900

    Answer

--- src/main/scala/work/ddd/Answer1.scala
+++ src/main/scala/work/ddd/Answer1.scala
@@ -3,9 +3,16 @@ package work.ddd
 import ixias.core.model.*
 
 object Answer1:
+
+  case class Member(
+    id: Option[Member.Id],
+    name: String,
+    updatedAt: LocalDateTime = Now,
+    createdAt: LocalDateTime = Now
+  ) extends EntityModel[Member.Id]
+
   object Member:
     object Id extends EntityId[Long]
     type Id = Id.Repr
 
   def main(args: Array[String]): Unit =
-    println(Member.Id(1001L).value)
