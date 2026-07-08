9d54f169e74e68688b6a5497c31631a3bb05fff0
名前       : (例) Kanta Ueno <k.ueno@hybrid-technologies.co.jp>
2026-07-08 16:32:48 +0900

    Answer

--- src/main/scala/work/ddd/Answer1.scala
+++ src/main/scala/work/ddd/Answer1.scala
@@ -6,16 +6,19 @@ import ixias.core.model.syntax.*
 object Answer1:
 
   case class Member(
-    id: Option[Member.Id],
-    name: String,
+    id:        Option[Member.Id],
+    name:      String,
+    email:     EmailAddress,
+    state:     Member.Status,
     updatedAt: LocalDateTime = Now,
     createdAt: LocalDateTime = Now
   ) extends EntityModel[Member.Id]
 
   object Member:
     object Id extends EntityId[Long]
-    type Id = Id.Repr
-    type WithNoId = Entity.WithNoId[Id, Member]
+    type Id         = Id.Repr
+    type WithNoId   = Entity.WithNoId[Id, Member]
+    type EmbeddedId = Entity.EmbeddedId[Id, Member]
 
     enum Status(val code: Int) extends EnumStatus[Int]:
       case Active    extends Status(1)
@@ -25,6 +28,20 @@ object Answer1:
 
   def main(args: Array[String]): Unit =
 
-    val draft = Member(id = None, name = "Alice").toWithNoId
-    println(draft.v.name)
+    val draft =
+      Member(
+        id    = None,
+        name  = "Alice",
+        email = EmailAddress("alice@example.com"),
+        state = Member.Status.Active
+      ).toWithNoId
 
+    val saved =
+      draft.v.copy(id = Some(Member.Id(1L))).toEmbeddedId
+
+    val alice =
+      saved.map(_.copy(state = Member.Status.Withdraw))
+
+    println(alice.id.asString)
+    println(alice.v.state)
+    println(alice.v.email)
