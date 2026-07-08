dd9185fba1918488c706038d852eb0ea65e7ef7b
名前       : (例) Kanta Ueno <k.ueno@hybrid-technologies.co.jp>
2026-07-08 16:05:51 +0900

    Answer

--- src/main/scala/work/ddd/Answer1.scala
+++ src/main/scala/work/ddd/Answer1.scala
@@ -1,6 +1,7 @@
 package work.ddd
 
 import ixias.core.model.*
+import ixias.core.model.syntax.*
 
 object Answer1:
 
@@ -14,5 +15,16 @@ object Answer1:
   object Member:
     object Id extends EntityId[Long]
     type Id = Id.Repr
+    type WithNoId = Entity.WithNoId[Id, Member]
+
+    enum Status(val code: Int) extends EnumStatus[Int]:
+      case Active    extends Status(1)
+      case Suspended extends Status(2)
+      case Withdraw  extends Status(3)
+
 
   def main(args: Array[String]): Unit =
+
+    val draft = Member(id = None, name = "Alice").toWithNoId
+    println(draft.v.name)
+
